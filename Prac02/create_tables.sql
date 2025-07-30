90e909b16a9d5c4679888bc0491097a29c94f6e9
yadkuriboh <yadshara@gmail.com>
2025-07-30 13:59:45 +0400

    Fix job column size from CHAR(8) to VARCHAR(20) to accommodate longer job titles

--- Prac02/create_tables.sql
+++ Prac02/create_tables.sql
@@ -1,32 +1,31 @@
+-- Drop existing tables to ensure clean creation
+DROP TABLE IF EXISTS Emp;
+DROP TABLE IF EXISTS Dept;
 
-/* createtables.sql: MySQL fle for table creation in DbS 2022 Practical-2 */
-
--- create dept table
---
- CREATE TABLE Dept
-               (deptno   CHAR(3),
-                deptname VARCHAR(36)    NOT NULL,
-                mgrno    CHAR(6),
-                admrdept CHAR(3)        NOT NULL,
-        	PRIMARY KEY(deptno)
+-- Create Department table
+CREATE TABLE Dept (
+    deptno CHAR(3) NOT NULL,
+    deptname VARCHAR(36) NOT NULL,
+    mgrno CHAR(6),
+    admrdept CHAR(3) NOT NULL,
+    PRIMARY KEY (deptno)
 );
 
--- create Emp table
---
- CREATE TABLE Emp
-               (empno     CHAR(6)		 ,
-                firstname VARCHAR(12)            ,
-                midinit	  CHAR(1)                ,
-                lastname  VARCHAR(15)            ,
-                workdept  CHAR(3)                ,
-                phoneno   CHAR(4)                ,
-                hiredate  DATE                   ,
-                job	  CHAR(10)               ,
-                edlevel	  INT(2)              	 ,
-                gender    CHAR(1)                ,
-                birthdate DATE                   ,
-                salary    DECIMAL(8,2)           ,
-                bonus     DECIMAL(8,2)           ,
-                comm      DECIMAL(8,2)		 ,
-		
+-- Create Employee table
+CREATE TABLE Emp (
+    empno CHAR(6) NOT NULL,
+    firstname VARCHAR(12) NOT NULL,
+    midinit CHAR(1) NOT NULL,
+    lastname VARCHAR(15) NOT NULL,
+    workdept CHAR(3),
+    phoneno CHAR(4),
+    hiredate DATE,
+    job CHAR(20),
+    edlevel INT,
+    gender CHAR(1),
+    birthdate DATE,
+    salary DECIMAL(8,2),
+    bonus DECIMAL(8,2),
+    comm DECIMAL(8,2),
+    PRIMARY KEY (empno)
 );
